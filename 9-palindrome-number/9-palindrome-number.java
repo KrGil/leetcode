db19c86f6e8f42690b4e4befbe87966c04b8c7af
Eisen <kanjjang89@naver.com>
2022-01-20 13:27:37 +0900

    Time: 66 ms (5.03%), Space: 42.7 MB (7.95%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,21 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         String[] sarr = String.valueOf(x).split("");
-        StringBuffer firstSb = new StringBuffer();
-        StringBuffer secondSb = new StringBuffer();
-        for (int i = 0; i < sarr.length/2; i++) {
-            firstSb.append(sarr[i]);
+         
+        int lastDigit = 0;
+        int tmp = x;
+        int reversedNum = 0;
+        while (tmp != 0) {
+            lastDigit = tmp % 10;
+            tmp = tmp / 10;
+            reversedNum = (reversedNum * 10) + lastDigit;
         }
-        for (int i = (sarr.length%2)== 0? (sarr.length/2) : (sarr.length/2)+1 ; i < sarr.length; i++) {
-            secondSb.append(sarr[i]);
-        }
-        secondSb = secondSb.reverse();
-        System.out.println("secondSb = " + secondSb.toString());
-        for (int i = 0; i < firstSb.length(); i++) {
-            if (firstSb.charAt(i) != secondSb.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
+        System.out.println("reversedNum = " + reversedNum);
+        return reversedNum==x && x >= 0?true:false;
     }
 }
